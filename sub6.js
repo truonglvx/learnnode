b84ad19d06a709494c577af75c0ed8509f58d7d9
truongaviron <truong.le@rowaviron.com>
2016-11-22 12:51:18 +0700

    8th time

---
+++ sub6.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const Nes = require('nes')
+
+const client = new Nes.Client('ws://104.197.189.66:8000')
+
+client.connect((err) => {
+  if (err) {
+    console.error(err.message)
+    process.exit(1)
+  }
+
+  console.log('connected')
+
+  client.subscribe('/echo4', (message) => {
+    console.log(message)
+  }, (err) => {
+    if (err) {
+      console.error(err.message)
+      process.exit(1)
+    }
+
+    console.log('subscribed')
+  })
+})
